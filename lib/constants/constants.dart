22ff6e4fa184c24a092a42bf8617fc63a2786862
Joe <tjoepaul@outlook.com>
2022-01-29 23:12:43 +0530

    first commit

---
+++ lib/constants/constants.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+const Color themeColor = Color(0xff5ac18e);
+const FOOD_DATA = [
+  {
+    "name":"TECHNICAL",
+    "image":"technical.png"
+  },
+  {
+    "name":"ARTS",
+    "image":"arts.png"
+  },
+  {
+    "name":"VIDEO GAMES",
+    "image":"games.png"
+  },
+  {
+    "name":"HACKATHONS",
+    "image":"hackathons.png"
+  }
+
+];
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:evently/screens/loginScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(LoginScreen());
+}
